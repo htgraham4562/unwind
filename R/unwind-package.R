542b42cd4022da0256a70e0ecf541eddf6cd02e4
htgraham4562 <120864336+htgraham4562@users.noreply.github.com>
2022-12-18 20:50:02 -0500

    update files

--- R/unwind-package.R
+++ R/unwind-package.R
@@ -1,10 +1,10 @@
-#' @import dplyr
-#' @import miniUI
 #' @import shiny
-#' @import shinyWidgets
-#' @import thematic
 #' @import magrittr
-#' @import lubridate
-#' @import ggplot2
-#' @import stringr
+#' @importFrom miniUI miniPage miniContentPanel
+#' @importFrom shinyWidgets panel setBackgroundColor switchInput
+#' @importFrom thematic thematic_on
+#' @importFrom dplyr mutate filter transmute select left_join group_by ungroup row_number
+#' @importFrom lubridate seconds_to_period
+#' @importFrom ggplot2 ggplot aes geom_point geom_text geom_hline geom_vline scale_x_continuous scale_y_continuous coord_fixed ggtitle theme element_blank element_text annotate theme_void
+#' @importFrom stringr str_count
 NULL
